ef5e2d636fa8b23aa721bb9f3126543e853060e1
Mihai <FilipCristiMihail@yahoo.com>
2022-12-04 21:33:28 +0100

    new Fragment - Concerts

---
+++ app/src/main/java/com/bandit/ui/concerts/ConcertsFragment.kt
@@ -0,0 +1,29 @@
+package com.bandit.ui.concerts
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bandit.ui.home.HomeViewModel
+import com.example.bandit.R
+import com.example.bandit.databinding.FragmentConcertsBinding
+
+class ConcertsFragment : Fragment() {
+
+    private lateinit var binding: FragmentConcertsBinding
+    private lateinit var viewModel: ConcertsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentConcertsBinding.inflate(layoutInflater, container, false)
+
+        viewModel = ViewModelProvider(this)[ConcertsViewModel::class.java]
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/bandit/ui/concerts/ConcertsViewModel.kt
@@ -0,0 +1,7 @@
+package com.bandit.ui.concerts
+
+import androidx.lifecycle.ViewModel
+
+class ConcertsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
