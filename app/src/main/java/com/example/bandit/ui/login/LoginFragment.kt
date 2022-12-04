f17a99769098a62a356f284eaed5ea1dad0ebc4e
Mihai <FilipCristiMihail@yahoo.com>
2022-12-04 20:27:59 +0100

    New Fragment Log In

---
+++ app/src/main/java/com/example/bandit/ui/login/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.example.bandit.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.bandit.R
+import com.example.bandit.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var _binding: FragmentLoginBinding
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
