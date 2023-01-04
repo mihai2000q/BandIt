99b94a41267e50cbf1ba775e85a8a9e3cecf1c31
Mihai <FilipCristiMihail@yahoo.com>
2023-01-05 00:20:40 +0100

    generation of concerts list in view model - initial

--- app/src/main/java/com/bandit/ui/concerts/ConcertsViewModel.kt
+++ app/src/main/java/com/bandit/ui/concerts/ConcertsViewModel.kt
@@ -1,7 +1,19 @@
 package com.bandit.ui.concerts
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bandit.data.MockDatabase
+import com.bandit.data.model.Concert
 
 class ConcertsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _concerts = MutableLiveData<List<Concert>>()
+    val concerts: LiveData<List<Concert>> get() = _concerts
+    init {
+        val mockDatabase = MockDatabase()
+        _concerts.value = mockDatabase.concerts
+    }
+    fun generateConcertsElements() {
+        
+    }
 }
