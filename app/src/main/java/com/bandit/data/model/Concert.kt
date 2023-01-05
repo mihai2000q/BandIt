813aef1b55ff6c326091e4425e8c43f245a18657
Mihai <FilipCristiMihail@yahoo.com>
2023-01-06 00:22:51 +0100

    Concert Sorting

--- app/src/main/java/com/bandit/data/model/Concert.kt
+++ app/src/main/java/com/bandit/data/model/Concert.kt
@@ -9,7 +9,7 @@ data class Concert(val name: String,
                    val city: String,
                    val country: String,
                    val place: String,
-                   val type: Type) : java.io.Serializable {
+                   val type: Type) : java.io.Serializable, Comparable<Concert> {
     enum class Type {Tournament, Simple, Festival}
     private val _id: Int = Generator.generateRandomConcertId()
     val id get() = _id
@@ -17,5 +17,14 @@ data class Concert(val name: String,
         fun getEmpty(): Concert = Concert("", LocalDateTime.now(),
             "", "", "", Type.Simple)
     }
+
+    override fun compareTo(other: Concert): Int {
+        return this.dateTime.compareTo(other.dateTime)
+    }
+
+    override fun toString(): String {
+        return "Concert(id=$id, name='$name', dateTime=$dateTime, city='$city', country='$country', place='$place', type=$type)"
+    }
+
 }
 
--- app/src/test/java/com/bandit/ConcertsTest.kt
+++ app/src/test/java/com/bandit/ConcertsTest.kt
@@ -8,6 +8,7 @@ import org.junit.Assert.*
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -42,5 +43,131 @@ class ConcertsTest {
             if (concert.id < 0 || concert.id > Constants.INT_MAX)
                 fail("The Id should be between these boundaries")
         }
+    }
+    @Test
+    fun concert_equal() {
+        val concert1 = Concert(
+            "Concert",
+            LocalDateTime.of(2020,10,25,20,30),
+            "Los Angeles",
+            "USA",
+            "Big Arena",
+            Concert.Type.Tournament
+        )
+        val concert2 = Concert(
+            "Concert",
+            LocalDateTime.of(2020,10,25,20,30),
+            "Los Angeles",
+            "USA",
+            "Big Arena",
+            Concert.Type.Tournament
+        )
+        assertEquals(concert1, concert2)
+        assertNotEquals(concert1.id, concert2.id)
+    }
+    @Test
+    fun concert_sort() {
+        val outcome = mutableListOf(
+            Concert(
+                "Concert1",
+                LocalDateTime.of(2020,10,25,20,30),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            ),
+            Concert(
+                "Concert2",
+                LocalDateTime.of(2021,10,25,20,30),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            ),
+            Concert(
+                "Concert3",
+                LocalDateTime.of(2023,10,25,20,30),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            ),
+            Concert(
+                "Concert4",
+                LocalDateTime.of(2020,1,23,20,30),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            ),
+            Concert(
+                "Concert5",
+                LocalDateTime.of(2020,10,25,20,29),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            ),
+            Concert(
+                "Concert6",
+                LocalDateTime.of(2020,10,25,19,29),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            )
+        )
+        outcome.sort()
+        val expected = listOf(
+            Concert(
+                "Concert4",
+                LocalDateTime.of(2020,1,23,20,30),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            ),
+            Concert(
+                "Concert6",
+                LocalDateTime.of(2020,10,25,19,29),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            ),
+            Concert(
+                "Concert5",
+                LocalDateTime.of(2020,10,25,20,29),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            ),
+            Concert(
+                "Concert1",
+                LocalDateTime.of(2020,10,25,20,30),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            ),
+            Concert(
+                "Concert2",
+                LocalDateTime.of(2021,10,25,20,30),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            ),
+            Concert(
+                "Concert3",
+                LocalDateTime.of(2023,10,25,20,30),
+                "",
+                "",
+                "",
+                Concert.Type.Tournament
+            )
+        )
+        assertEquals(outcome, expected)
     }
 }
