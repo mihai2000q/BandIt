6cca871eb2426b5893662944c55addfe56265f48
Mihai <FilipCristiMihail@yahoo.com>
2023-01-11 16:36:12 +0100

    nullable

--- app/src/main/java/com/bandit/builder/HomeBuilder.kt
+++ app/src/main/java/com/bandit/builder/HomeBuilder.kt
@@ -10,7 +10,7 @@ interface HomeBuilder {
     fun buildHomeButton(
         context: Context,
         entry: Map.Entry<String, NavigationType>,
-        bottomNav: BottomNavigationView
+        bottomNav: BottomNavigationView?
     ): Button
     fun buildTableRow(context: Context, tag: String): TableRow
 }
