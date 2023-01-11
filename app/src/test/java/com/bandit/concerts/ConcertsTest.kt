dd1d40246d6d28ed96924dd066fc78b7aaa2a589
Mihai <FilipCristiMihail@yahoo.com>
2023-01-11 15:58:33 +0100

    cleanup

--- app/src/test/java/com/bandit/concerts/ConcertsTest.kt
+++ app/src/test/java/com/bandit/concerts/ConcertsTest.kt
@@ -7,11 +7,6 @@ import org.junit.Test
 import org.junit.Assert.*
 import java.time.LocalDateTime
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ConcertsTest {
     @Test
     fun concert_init() {
