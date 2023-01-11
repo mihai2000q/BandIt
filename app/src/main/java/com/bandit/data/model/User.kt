ddf2be9b36cf9c2d9062962d6447a73fdfe2d91e
Mihai <FilipCristiMihail@yahoo.com>
2023-01-11 23:46:05 +0100

    new Classes - User + Settings

---
+++ app/src/main/java/com/bandit/data/model/User.kt
@@ -0,0 +1,35 @@
+package com.bandit.data.model
+
+import com.bandit.helper.AndroidUtils
+
+data class User(
+    val username: String,
+    val password: String,
+    val settings: Settings,
+    private val _id: Int = AndroidUtils.generateRandomId()
+    ) {
+    val id get() = _id
+
+    override fun toString(): String {
+        return "User(id=$id, username='$username', password='$password')"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        if (username != other.username) return false
+        if (password != other.password) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = username.hashCode()
+        result = 31 * result + password.hashCode()
+        return result
+    }
+
+}
