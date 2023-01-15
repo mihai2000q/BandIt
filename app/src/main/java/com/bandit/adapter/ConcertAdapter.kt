2ae9ba7f821484d5eca7c1a07389a72bcac5fa88
Mihai <FilipCristiMihail@yahoo.com>
2023-01-15 19:23:33 +0100

    cleanup

--- app/src/main/java/com/bandit/adapter/ConcertAdapter.kt
+++ app/src/main/java/com/bandit/adapter/ConcertAdapter.kt
@@ -11,7 +11,6 @@ import com.bandit.data.model.Concert
 import com.bandit.databinding.ModelConcertBinding
 import com.bandit.constant.BandItEnums
 import com.bandit.extension.StringExtensions.normalizeWord
-import java.time.LocalDateTime
 
 class ConcertAdapter(
     private val concerts: List<Concert>,
@@ -37,42 +36,44 @@ class ConcertAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val concert = concerts[position]
 
-        holder.itemView.setOnClickListener { onConcertClick(concert) }
-        holder.itemView.setOnLongClickListener {
-            val popupMenu = PopupMenu(holder.binding.root.context, holder.itemView)
-            popupMenu.inflate(R.menu.item_popup_menu)
-            popupMenu.setOnMenuItemClickListener {
-                popupMenu.dismiss()
-                when(it.itemId) {
-                    R.id.popup_menu_delete -> {
-                        onDeleteItem(concert)
-                    }
-                    else -> {
-                        onEditItem(concert)
+        with(holder) {
+            itemView.setOnClickListener { onConcertClick(concert) }
+            itemView.setOnLongClickListener {
+                val popupMenu = PopupMenu(binding.root.context, itemView)
+                popupMenu.inflate(R.menu.item_popup_menu)
+                popupMenu.setOnMenuItemClickListener {
+                    popupMenu.dismiss()
+                    when (it.itemId) {
+                        R.id.popup_menu_delete -> {
+                            onDeleteItem(concert)
+                        }
+                        else -> {
+                            onEditItem(concert)
+                        }
                     }
                 }
+                popupMenu.show()
+                onConcertLongClick(concert)
             }
-            popupMenu.show()
-            onConcertLongClick(concert)
-        }
 
-        with(holder.binding) {
-            when(concert.type) {
-                BandItEnums.Concert.Type.Simple -> concertLayout.setBackgroundColor(Color.CYAN)
-                BandItEnums.Concert.Type.Tournament -> concertLayout.setBackgroundColor(Color.RED)
-                BandItEnums.Concert.Type.Festival -> concertLayout.setBackgroundColor(Color.GREEN)
-            }
-            concertTitle.text = concert.name.uppercase()
-            concertCityCountry.text = "${concert.city.normalizeWord()}, " +
-                    concert.country.normalizeWord()
-            concertPlace.text = concert.place
-            concertDate.text = when {
-                concert.is7DaysApart() -> concert.dateTime.dayOfWeek.name.normalizeWord()
-                concert.isOneYearApart() -> "${concert.dateTime.dayOfMonth} " +
-                        concert.dateTime.month.name.substring(0..2).normalizeWord() +
-                        " ${concert.dateTime.year}"
-                else -> "${concert.dateTime.dayOfMonth} " +
-                        concert.dateTime.month.name.normalizeWord()
+            with(binding) {
+                when (concert.type) {
+                    BandItEnums.Concert.Type.Simple -> concertLayout.setBackgroundColor(Color.CYAN)
+                    BandItEnums.Concert.Type.Tournament -> concertLayout.setBackgroundColor(Color.RED)
+                    BandItEnums.Concert.Type.Festival -> concertLayout.setBackgroundColor(Color.GREEN)
+                }
+                concertTitle.text = concert.name.uppercase()
+                concertCityCountry.text = "${concert.city.normalizeWord()}, " +
+                        concert.country.normalizeWord()
+                concertPlace.text = concert.place
+                concertDate.text = when {
+                    concert.is7DaysApart() -> concert.dateTime.dayOfWeek.name.normalizeWord()
+                    concert.isOneYearApart() -> "${concert.dateTime.dayOfMonth} " +
+                            concert.dateTime.month.name.substring(0..2).normalizeWord() +
+                            " ${concert.dateTime.year}"
+                    else -> "${concert.dateTime.dayOfMonth} " +
+                            concert.dateTime.month.name.normalizeWord()
+                }
             }
         }
     }
