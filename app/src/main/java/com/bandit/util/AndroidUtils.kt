e2b8e84f379d1d1e925c3207bd21b4e12b540890
Mihai <FilipCristiMihail@yahoo.com>
2023-01-16 12:35:40 +0100

    Update AndroidUtils

--- app/src/main/java/com/bandit/util/AndroidUtils.kt
+++ app/src/main/java/com/bandit/util/AndroidUtils.kt
@@ -5,25 +5,27 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.bandit.R
 import com.bandit.constant.Constants
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlin.random.Random
 
 object AndroidUtils {
     fun generateRandomId() = Random.nextInt(Constants.MAX_NR_ITEMS)
+    fun unlockNavigation(bottomNavigationView: BottomNavigationView?, drawerLayout: DrawerLayout?) {
+        bottomNavigationView?.visibility = View.VISIBLE
+        drawerLayout?.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+    }
+    fun lockNavigation(bottomNavigationView: BottomNavigationView?, drawerLayout: DrawerLayout?) {
+        drawerLayout?.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        bottomNavigationView?.visibility = View.INVISIBLE
+    }
     //Context.INPUT_METHOD_SERVICE
     fun hideKeyboard(activity: Activity, inputMethodService: String, view: View) {
         val input = activity.getSystemService(inputMethodService) as InputMethodManager
         input.hideSoftInputFromWindow(view.windowToken, 0)
-    }
-    fun savePreference(activity: Activity, key: String ,value: Any) {
-        val editor: SharedPreferences.Editor = activity
-            .getSharedPreferences(Constants.Preferences.APP_PREFERENCES, Context.MODE_PRIVATE).edit()
-        editor.putBoolean(key, value as Boolean)
-        editor.apply()
-    }
-    fun getBooleanPreference(activity: Activity, key: String): Boolean {
-        return activity
-            .getSharedPreferences(Constants.Preferences.APP_PREFERENCES, Context.MODE_PRIVATE)
-            .getBoolean(key, false)
     }
 }
