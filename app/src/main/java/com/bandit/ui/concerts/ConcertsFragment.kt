13dfbf08e12c621d63d59485ce39076d8f36c6e4
Mihai <FilipCristiMihail@yahoo.com>
2023-01-17 19:03:52 +0100

    update Home and Concert Fragment

--- app/src/main/java/com/bandit/ui/concerts/ConcertsFragment.kt
+++ app/src/main/java/com/bandit/ui/concerts/ConcertsFragment.kt
@@ -1,48 +1,46 @@
 package com.bandit.ui.concerts
 
-import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import com.bandit.adapter.ConcertAdapter
 import com.bandit.databinding.FragmentConcertsBinding
 
 class ConcertsFragment : Fragment() {
 
-    private var binding: FragmentConcertsBinding? = null
+    private var _binding: FragmentConcertsBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: ConcertsViewModel by activityViewModels()
     private lateinit var detailFragment: ConcertDetailDialogFragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentConcertsBinding.inflate(inflater, container, false)
-
-        binding?.concertsBtAdd?.setOnClickListener {
-            ConcertAddDialogFragment().show(childFragmentManager, ConcertAddDialogFragment.TAG)
-        }
-        binding?.concertsBtFilter?.setOnClickListener {
-            ConcertFilterDialogFragment().show(childFragmentManager, ConcertFilterDialogFragment.TAG)
-        }
-        detailFragment = ConcertDetailDialogFragment()
-
-        return binding?.root
+    ): View {
+        _binding = FragmentConcertsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.concertsBtAdd.setOnClickListener {
+            ConcertAddDialogFragment().show(childFragmentManager, ConcertAddDialogFragment.TAG)
+        }
+        binding.concertsBtFilter.setOnClickListener {
+            ConcertFilterDialogFragment().show(childFragmentManager, ConcertFilterDialogFragment.TAG)
+        }
+        detailFragment = ConcertDetailDialogFragment()
+
         viewModel.concerts.observe(viewLifecycleOwner) {
-            binding?.concertsList?.adapter = ConcertAdapter(it.sorted(), { concert ->
+            binding.concertsList.adapter = ConcertAdapter(it.sorted(), { concert ->
                 viewModel.selectedConcert.value = concert
                 detailFragment.show(childFragmentManager, ConcertDetailDialogFragment.TAG) },
-            { concert -> viewModel.selectedConcert.value = concert; return@ConcertAdapter true },
-            { concert -> return@ConcertAdapter viewModel.removeConcert(concert) }) { concert ->
+                { concert -> viewModel.selectedConcert.value = concert; return@ConcertAdapter true },
+                { concert -> return@ConcertAdapter viewModel.removeConcert(concert) }) { concert ->
                 viewModel.selectedConcert.value = concert
                 ConcertEditDialogFragment().show(childFragmentManager, ConcertEditDialogFragment.TAG)
                 return@ConcertAdapter true
@@ -52,6 +50,6 @@ class ConcertsFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding = null
+        _binding = null
     }
 }
--- app/src/main/java/com/bandit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bandit/ui/home/HomeFragment.kt
@@ -11,35 +11,38 @@ import com.bandit.di.DILocator
 
 class HomeFragment : Fragment() {
 
-    private var binding: FragmentHomeBinding? = null
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: HomeViewModel by activityViewModels()
 
-    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel.generateHomeElements(
             viewModel.elements.value ?: mapOf(),
-            binding!!.homeSvTableLayout,
+            binding.homeSvTableLayout,
             this.requireContext(),
             this.requireActivity().findViewById(R.id.main_bottom_navigation_view)
         )
 
-        binding?.homeBtAccount?.setOnClickListener{
+        binding.homeBtAccount.setOnClickListener{
             HomeAccountDialogFragment().show(childFragmentManager, HomeAccountDialogFragment.TAG)
         }
 
-        binding?.homeTvWelcome?.text = "Welcome ${DILocator.authenticator.currentUser!!.displayName}, to"
-
-        return binding?.root
+        binding.homeTvWelcome.text = "Welcome ${DILocator.authenticator.currentUser!!.displayName}, to"
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding = null
+        _binding = null
     }
 
 }
