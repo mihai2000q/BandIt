04af49e1b58b00d207c61be57a700884ff6c6a89
Mihai <FilipCristiMihail@yahoo.com>
2023-01-19 10:33:48 +0100

    new String extension method

--- app/src/main/java/com/bandit/extension/StringExtensions.kt
+++ app/src/main/java/com/bandit/extension/StringExtensions.kt
@@ -3,4 +3,6 @@ package com.bandit.extension
 object StringExtensions {
     fun String.normalizeWord() =
         this[0].uppercase() + this.substring(1).lowercase()
+    fun String.get2Characters() =
+        if(this.length == 1) "0$this" else this
 }
