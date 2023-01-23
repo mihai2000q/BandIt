7c153e8fda82ec54ab4099af5a07ac33c09c80e6
Mihai <FilipCristiMihail@yahoo.com>
2023-01-23 21:08:15 +0100

    Update methods in Authenticator - division between updating and creating user

--- app/src/main/java/com/bandit/ui/signup/SignupViewModel.kt
+++ app/src/main/java/com/bandit/ui/signup/SignupViewModel.kt
@@ -9,13 +9,11 @@ import kotlinx.coroutines.launch
 class SignupViewModel : ViewModel() {
     val email = MutableLiveData<String>()
     val password = MutableLiveData<String>()
-    val displayName = MutableLiveData<String>()
     fun createUser() {
         viewModelScope.launch {
             DILocator.authenticator.createUser(
                 email.value!!,
-                password.value!!,
-                displayName.value!!
+                password.value!!
             )
         }
     }
