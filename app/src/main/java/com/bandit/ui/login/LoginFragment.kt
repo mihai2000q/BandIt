15dfc92c28b4f81c9a10d6e0450fafc9e54a2ef4
Mihai <FilipCristiMihail@yahoo.com>
2023-01-24 00:44:45 +0100

    Log in Fragment searches for an account if its setup

--- app/src/main/java/com/bandit/ui/login/LoginFragment.kt
+++ app/src/main/java/com/bandit/ui/login/LoginFragment.kt
@@ -14,6 +14,7 @@ import com.bandit.R
 import com.bandit.constant.Constants
 import com.bandit.databinding.FragmentLoginBinding
 import com.bandit.di.DILocator
+import com.bandit.ui.first.login.FirstLoginViewModel
 import com.bandit.util.AndroidUtils
 import com.bandit.util.PreferencesUtils
 import kotlinx.coroutines.*
@@ -64,23 +65,36 @@ class LoginFragment : Fragment() {
             Context.INPUT_METHOD_SERVICE,
             binding.fragmentLoginBtLogin
         )
-        PreferencesUtils.savePreference(
-            super.requireActivity(),
-            Constants.Preferences.REMEMBER_ME,
-            binding.fragmentLoginCbRemember.isChecked
-        )
-        AndroidUtils.unlockNavigation(
-            super.requireActivity().findViewById(R.id.main_bottom_navigation_view),
-            super.requireActivity().findViewById(R.id.main_drawer_layout)
-        )
+        var result: Boolean? = null
         lifecycleScope.launch {
-            DILocator.database.init()
+            launch { result = DILocator.database.isUserAccountSetup() }.join()
+
+            if (result == true) {
+                launch { DILocator.database.init() }.join()
+                PreferencesUtils.savePreference(
+                    super.requireActivity(),
+                    Constants.Preferences.REMEMBER_ME,
+                    binding.fragmentLoginCbRemember.isChecked
+                )
+                AndroidUtils.unlockNavigation(
+                    super.requireActivity().findViewById(R.id.main_bottom_navigation_view),
+                    super.requireActivity().findViewById(R.id.main_drawer_layout)
+                )
+                findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+            } else if (result == false) {
+                findNavController().navigate(R.id.action_navigation_login_to_firstLoginFragment)
+                val firstLoginViewModel: FirstLoginViewModel by activityViewModels()
+                firstLoginViewModel.rememberMe.value = binding.fragmentLoginCbRemember.isChecked
+                AndroidUtils.lockNavigation(
+                    super.requireActivity().findViewById(R.id.main_bottom_navigation_view),
+                    super.requireActivity().findViewById(R.id.main_drawer_layout)
+                )
+            }
+            AndroidUtils.toastNotification(
+                super.requireContext(),
+                resources.getString(R.string.Login_Toast),
+                Toast.LENGTH_LONG
+            )
         }
-        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
-        AndroidUtils.toastNotification(
-            this.requireContext(),
-            resources.getString(R.string.Login_Toast),
-            Toast.LENGTH_LONG
-        )
     }
 }
