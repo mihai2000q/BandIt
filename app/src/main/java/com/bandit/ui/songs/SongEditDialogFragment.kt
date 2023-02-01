3738dca01529cd8bc0dc3fc45c55634f85f4367f
Mihai <FilipCristiMihail@yahoo.com>
2023-02-01 17:27:49 +0100

    small fix

--- app/src/main/java/com/bandit/ui/songs/SongEditDialogFragment.kt
+++ app/src/main/java/com/bandit/ui/songs/SongEditDialogFragment.kt
@@ -5,6 +5,8 @@ import android.view.View
 import com.bandit.R
 import com.bandit.constant.Constants
 import com.bandit.data.model.Song
+import com.bandit.extension.print
+import com.bandit.extension.printName
 import com.bandit.util.ParserUtils
 
 class SongEditDialogFragment : SongDialogFragment() {
@@ -15,7 +17,7 @@ class SongEditDialogFragment : SongDialogFragment() {
             viewModel.selectedSong.observe(viewLifecycleOwner) {
                 songEtName.setText(it.name)
                 songEtReleaseDate.setText(it.releaseDate.toString())
-                songEtDuration.setText(it.duration.toString())
+                songEtDuration.setText(it.duration.print())
             }
             songButton.setOnClickListener {
                 viewModel.editSong(
