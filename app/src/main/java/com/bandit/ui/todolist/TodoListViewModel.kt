b5d89fa9e1dd4ae5bb02a42c571aed9c2fcd61e0
Mihai <FilipCristiMihail@yahoo.com>
2023-02-06 23:30:11 +0100

    To Do List View Model

--- app/src/main/java/com/bandit/ui/todolist/TodoListViewModel.kt
+++ app/src/main/java/com/bandit/ui/todolist/TodoListViewModel.kt
@@ -1,7 +1,42 @@
 package com.bandit.ui.todolist
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bandit.constant.Constants
+import com.bandit.data.model.Task
+import com.bandit.data.repository.TaskRepository
+import com.bandit.di.DILocator
+import kotlinx.coroutines.launch
 
 class TodoListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _database = DILocator.database
+    private val _repository = TaskRepository(_database)
+    private val _tasks = MutableLiveData(_repository.list)
+    val tasks: LiveData<List<Task>> = _tasks
+    fun addTask(task: Task) {
+        viewModelScope.launch {
+            launch { _repository.add(task) }.join()
+            _tasks.value = _repository.list
+        }
+    }
+    fun removeTask(task: Task): Boolean {
+        var result = false
+        viewModelScope.launch {
+            launch { result = _repository.remove(task) }.join()
+            _tasks.value = _repository.list
+        }
+        return result
+    }
+    fun editTask(task: Task) {
+        viewModelScope.launch {
+            launch { _repository.edit(task) }.join()
+            _tasks.value = _repository.list
+        }
+    }
+
+    companion object {
+        const val TAG = Constants.ToDoList.VIEW_MODEL_TAG
+    }
 }
