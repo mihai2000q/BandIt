ae52949ea65e7985841d902e6fdb52c5a8e5c251
Mihai <FilipCristiMihail@yahoo.com>
2023-02-07 23:53:20 +0100

    Personal Notes Fragment + view model

---
+++ app/src/main/java/com/bandit/ui/notes/PersonalNotesFragment.kt
@@ -0,0 +1,36 @@
+package com.bandit.ui.notes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.bandit.databinding.FragmentPersonalNotesBinding
+
+class PersonalNotesFragment : Fragment() {
+
+    private var _binding: FragmentPersonalNotesBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: PersonalNotesViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPersonalNotesBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/bandit/ui/notes/PersonalNotesViewModel.kt
@@ -0,0 +1,7 @@
+package com.bandit.ui.notes
+
+import androidx.lifecycle.ViewModel
+
+class PersonalNotesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
