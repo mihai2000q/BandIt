f5db6c1085bb81c1717179aafd65960a387c1925
Mihai <FilipCristiMihail@yahoo.com>
2023-02-08 21:01:08 +0100

    Album and Song Remember filters while navigating

--- app/src/main/java/com/bandit/ui/songs/albums/AlbumFilterDialogFragment.kt
+++ app/src/main/java/com/bandit/ui/songs/albums/AlbumFilterDialogFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import com.bandit.R
 import com.bandit.constant.Constants
+import com.bandit.ui.songs.SongsViewModel
 import com.bandit.util.AndroidUtils
 import com.bandit.util.ParserUtils
 
@@ -12,6 +13,12 @@ class AlbumFilterDialogFragment : AlbumDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
+            val map = mapOf(
+                albumEtName to SongsViewModel.AlbumFilter.Name,
+                albumEtReleaseDate to SongsViewModel.AlbumFilter.ReleaseDate,
+                albumEtLabel to SongsViewModel.AlbumFilter.Label,
+            )
+            map.forEach { (key, value) -> key.setText(viewModel.albumFilters.value?.get(value)) }
             albumButton.setText(R.string.bt_filter)
             albumButton.setOnClickListener {
                 viewModel.filterAlbums(
@@ -22,6 +29,9 @@ class AlbumFilterDialogFragment : AlbumDialogFragment() {
                         ParserUtils.parseDate(albumEtReleaseDate.text.toString()),
                     albumEtLabel.text.toString()
                 )
+                map.forEach { (key, value) ->
+                    viewModel.albumFilters.value?.replace(value, key.text.toString())
+                }
                 AndroidUtils.toastNotification(
                     super.requireContext(),
                     resources.getString(R.string.album_filter_toast)
