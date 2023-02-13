263accefbeb404dcab6fda49d23b65a5c52db133
Mihai <FilipCristiMihail@yahoo.com>
2023-02-13 12:06:05 +0100

    new Fragment and View Model - Friends

---
+++ app/src/main/java/com/bandit/ui/friends/FriendsFragment.kt
@@ -0,0 +1,37 @@
+package com.bandit.ui.friends
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.bandit.databinding.FragmentFriendsBinding
+
+class FriendsFragment : Fragment() {
+
+    private var _binding: FragmentFriendsBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: FriendsViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFriendsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/bandit/ui/friends/FriendsViewModel.kt
@@ -0,0 +1,11 @@
+package com.bandit.ui.friends
+
+import androidx.lifecycle.ViewModel
+import com.bandit.constant.Constants
+
+class FriendsViewModel : ViewModel() {
+
+    companion object {
+        const val TAG = Constants.Social.Friends.VIEW_MODEL_TAG
+    }
+}
