b97f4612ede8dab73648abe745b49bb5bb094358
Mihai <FilipCristiMihail@yahoo.com>
2023-02-14 01:34:45 +0100

    Friends Fragment setup + new friends Dialog fragment and functionalities + resources + new adapter for People (new friends) and its model

--- app/src/main/java/com/bandit/ui/friends/FriendsFragment.kt
+++ app/src/main/java/com/bandit/ui/friends/FriendsFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.bandit.databinding.FragmentFriendsBinding
+import com.bandit.ui.adapter.PeopleAdapter
+import com.bandit.util.AndroidUtils
 
-class FriendsFragment : Fragment() {
-
+class FriendsFragment : Fragment(), OnQueryTextListener {
     private var _binding: FragmentFriendsBinding? = null
     private val binding get() = _binding!!
     private val viewModel: FriendsViewModel by activityViewModels()
+    private val friendsNewDialogFragment = FriendsNewDialogFragment()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,12 +29,32 @@ class FriendsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
+            friendsBtAddNewFriends.setOnClickListener {
+                AndroidUtils.showDialogFragment(
+                    friendsNewDialogFragment,
+                    childFragmentManager
+                )
+            }
+            friendsBtAdd.setOnClickListener {
 
+            }
+            friendsSearchView.setOnQueryTextListener(this@FriendsFragment)
+            viewModel.friends.observe(viewLifecycleOwner) {
+                friendsList.adapter = PeopleAdapter(it.sorted())
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/bandit/ui/friends/FriendsNewDialogFragment.kt
@@ -0,0 +1,66 @@
+package com.bandit.ui.friends
+
+import android.app.ActionBar
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView.OnQueryTextListener
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import com.bandit.R
+import com.bandit.constant.Constants
+import com.bandit.databinding.DialogFragmentFriendsBinding
+import com.bandit.ui.adapter.PeopleAdapter
+import com.bandit.util.AndroidUtils
+
+class FriendsNewDialogFragment : DialogFragment(), OnQueryTextListener {
+    private var _binding: DialogFragmentFriendsBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: FriendsViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogFragmentFriendsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.dialog?.window?.setLayout(
+            AndroidUtils.getScreenWidth(super.requireActivity()),
+            ActionBar.LayoutParams.WRAP_CONTENT
+        )
+        viewModel.people.observe(viewLifecycleOwner) {
+            binding.friendsNewList.adapter = PeopleAdapter(it.sorted()) { acc ->
+                viewModel.sendFriendRequest(acc)
+                AndroidUtils.toastNotification(
+                    super.requireContext(),
+                    resources.getString(R.string.friend_request_sent_toast)
+                )
+                super.dismiss()
+            }
+        }
+        binding.friendsNewSearchView.setOnQueryTextListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    companion object {
+        const val TAG = Constants.Social.Friends.NEW_FRIEND_TAG
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+}
