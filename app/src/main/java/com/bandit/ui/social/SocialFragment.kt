21dca32a0aeb454f6f2155d6e5c9a956608d4c32
Mihai <FilipCristiMihail@yahoo.com>
2023-02-14 10:55:38 +0100

    Badge drawable works on social fragment

--- app/src/main/java/com/bandit/ui/social/SocialFragment.kt
+++ app/src/main/java/com/bandit/ui/social/SocialFragment.kt
@@ -5,15 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.bandit.R
 import com.bandit.databinding.FragmentSocialBinding
 import com.bandit.ui.adapter.SocialViewPagerAdapter
+import com.bandit.ui.friends.FriendsViewModel
 import com.google.android.material.tabs.TabLayoutMediator
 
 class SocialFragment : Fragment() {
 
     private var _binding: FragmentSocialBinding? = null
     private val binding get() = _binding!!
+    private val friendsViewModel: FriendsViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +33,14 @@ class SocialFragment : Fragment() {
             TabLayoutMediator(socialTabLayout, socialViewPager) { tab, pos ->
                 when(pos) {
                     0 -> tab.text = resources.getString(R.string.social_chats_tab)
-                    else -> tab.text = resources.getString(R.string.social_friends_tab)
+                    else -> {
+                        tab.text = resources.getString(R.string.social_friends_tab)
+                        friendsViewModel.friendRequests.observe(viewLifecycleOwner) {
+                            val badge = tab.orCreateBadge
+                            badge.isVisible = it.isNotEmpty()
+                            badge.number = it.size
+                        }
+                    }
                 }
             }.attach()
         }
