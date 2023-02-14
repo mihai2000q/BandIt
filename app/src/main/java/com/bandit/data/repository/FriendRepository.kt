417b1e0db1f86b683e1200ddeaa9068cec9727f8
Mihai <FilipCristiMihail@yahoo.com>
2023-02-14 13:09:02 +0100

    friend repo fixes

--- app/src/main/java/com/bandit/data/repository/FriendRepository.kt
+++ app/src/main/java/com/bandit/data/repository/FriendRepository.kt
@@ -9,23 +9,23 @@ class FriendRepository(val database: Database? = null) {
     val people: List<Account> = _people
     private val _friends: MutableList<Account> = database?.friends?.toMutableList() ?: mutableListOf()
     val friends: List<Account> = _friends
-    private val _friendRequests: MutableList<Account> = database?.friends?.toMutableList() ?: mutableListOf()
+    private val _friendRequests: MutableList<Account> = database?.friendRequests?.toMutableList() ?: mutableListOf()
     val friendRequests: List<Account> = _friendRequests
 
     suspend fun sendFriendRequest(account: Account) {
         database?.sendFriendRequest(account)
-        _friendRequests.add(account)
         _people.remove(account)
     }
 
     suspend fun acceptFriendRequest(account: Account) {
-        database?.sendFriendRequest(account)
+        database?.acceptFriendRequest(account)
         _friendRequests.remove(account)
+        _people.remove(account)
         _friends.add(account)
     }
 
     suspend fun rejectFriendRequest(account: Account) {
-        database?.sendFriendRequest(account)
+        database?.rejectFriendRequest(account)
         _friendRequests.remove(account)
         _people.add(account)
     }
