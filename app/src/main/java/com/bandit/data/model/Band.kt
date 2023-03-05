eedca229c2090ff0a22fef11beced3d754598a7f
Mihai <FilipCristiMihail@yahoo.com>
2023-03-05 03:30:46 +0100

    Unit Testing - Band Model

--- app/src/main/java/com/bandit/data/model/Band.kt
+++ app/src/main/java/com/bandit/data/model/Band.kt
@@ -8,7 +8,6 @@ data class Band(
     val creator: Long,
     val members: MutableMap<Account, Boolean>,
     override val id: Long = AndroidUtils.generateRandomLong()
-) : Item(id) {
 ) : Item(id), Comparable<Band> {
     fun isEmpty(): Boolean {
         return this == EMPTY
@@ -17,6 +16,31 @@ data class Band(
     override fun compareTo(other: Band): Int {
         return this.name.compareTo(other.name)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Band
+
+        if (name != other.name) return false
+        if (creator != other.creator) return false
+        if (members != other.members) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + creator.hashCode()
+        result = 31 * result + members.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Band(id=$id, name='$name', creator=$creator, members=$members)"
+    }
+
     companion object {
         val EMPTY = Band("", -1, mutableMapOf())
     }
