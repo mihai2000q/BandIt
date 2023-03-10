121382945b07c05cf5802a7fb7bff6e1bd58cf71
Mihai <FilipCristiMihail@yahoo.com>
2023-03-10 23:20:07 +0100

    Rounded Corners for Dialog Fragments and Image Picker

--- app/src/main/java/com/bandit/ui/component/ImagePickerDialog.kt
+++ app/src/main/java/com/bandit/ui/component/ImagePickerDialog.kt
@@ -35,6 +35,11 @@ class ImagePickerDialog(
     private lateinit var galleryPermissionLauncher: ActivityResultLauncher<String>
     private lateinit var permissionService: IPermissionService
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        this.setStyle(STYLE_NORMAL, R.style.AppBottomSheetDialogTheme)
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
